3d03cd00a458207c97e907276af3a426059f6c91
deleomike <michaeldeleo31@yahoo.com>
2020-09-13 17:57:55 -0400

    Updated cuda script

--- scripts/installCuda102.sh
+++ scripts/installCuda102.sh
@@ -1,23 +1,20 @@
 #!/usr/bin
 
-INSTALLER=1
+echo "NVIDIA GPUS"
+lspci | grep -i nvidia
 
-if [INSTALLER == 0] then
-	#Manual install, not always the best because it does not leave an uninstaller
-	wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+echo "This install is for x86_64 Ubuntu 18. You have:"
+uname -m && cat /etc/*release
 
-	sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+echo "Kernel Version"
+uname -r
 
-	sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+echo "Choose to install these headers"
+sudo apt-get install linux-headers-$(uname -r)
 
-	sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
-
-	sudo apt-get update -y
-
-	sudo apt-get -y install cuda
-else
-	#Installer route
-	wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run
-	sudo sh cuda_10.2.89_440.33.01_linux.run
-	
-fi
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
+sudo apt-get update
+sudo apt-get -y install cuda
