198d4a469a7ff501fadbbf4cfa8fc520d71c42b0
Michael DeLeo <michaeldeleo31@yahoo.com>
2020-09-14 16:53:38 -0400

    Changes to cuda script. Helper Scripts is functional now

--- installers/cuda102.sh
+++ installers/cuda102.sh
@@ -18,15 +18,18 @@ uname -r
 echo "Choose to install these headers"
 sudo apt-get install -y linux-headers-$(uname -r)
 
-sudo -s
 echo "blacklist nouveau
-options nouveau modeset=0" > /etc/modprobe.d/blacklist-nouveau.conf
-exit
+options nouveau modeset=0" | sudo tee /etc/modprobe.d/blacklist-nvidia-nouveau.conf
 
 sudo update-initramfs -u
 
 wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run
 
-sudo sh cuda_10.2.89_440.33.01_linux.run --silent --driver --toolkit --samples
+mkdir ~/tmp
+sudo bash cuda_10.2.89_440.33.01_linux.run --tmpdir=~/tmp --silent --driver --toolkit --samples
 
+echo export PATH="/usr/local/cuda-10.2/bin:$PATH" >> ~/.bashrc
+echo export LD_LIBRARY_PATH="/usr/local/cuda-10.2/lib64:$LD_LIBRARY_PATH" >> ~/.bashrc
+
+echo "CUDA 10.2 Installed"
 
