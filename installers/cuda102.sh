10c67b6b1a5598c90b77bca2c9841867072cc8a7
Michael DeLeo <michaeldeleo31@yahoo.com>
2020-09-13 23:15:51 -0400

    Updates to cuda script

--- installers/cuda102.sh
+++ installers/cuda102.sh
@@ -18,9 +18,15 @@ uname -r
 echo "Choose to install these headers"
 sudo apt-get install linux-headers-$(uname -r)
 
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
-sudo apt-get update
-sudo apt-get -y install cuda
+sudo -s
+echo "blacklist nouveau
+options nouveau modeset=0" > /etc/modprobe.d/blacklist-nouveau.conf
+exit
+
+sudo update-initramfs -u
+
+wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run
+
+sudo sh cuda_10.2.89_440.33.01_linux.run --silent --driver --toolkit --samples
+
+
